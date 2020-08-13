de3137cc0d1c00f62dc0e28384d7b58d0345e022
Raziel <raziel@codefresh.io>
2020-08-13 11:46:52 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Expressoo' });
+  res.render('index', { title: 'Expresso' });
 });
 
 module.exports = router;
